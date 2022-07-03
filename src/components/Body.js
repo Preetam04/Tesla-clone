7e45d1bc8aece4efca809f3d6d37e61f04a50432
Preetam04 <preetampatil2324@gmail.com>
2022-07-03 09:34:50 +0530

    background image added

---
+++ src/components/Body.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BgImage from "./BgImage";
+
+function Body() {
+  return (
+    <div>
+      <BgImage>Hellow</BgImage>
+    </div>
+  );
+}
+
+export default Body;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Header() {
-  return <div className=""></div>;
+  return <div></div>;
 }
 
 export default Header;
