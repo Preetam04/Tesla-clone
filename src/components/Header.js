1e911c1c3d4346d49ffd2f275b85ef7077334100
Preetam04 <preetampatil2324@gmail.com>
2022-07-11 13:00:32 +0530

    created tesla-header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,39 @@
 import React from "react";
 
 function Header() {
+  const style = {
+    container:
+      "absolute z-10 left-0 right-0 flex items-center justify-between mx-3  xl:mx-5 mt-2.5",
+    navEle:
+      "font-Gottham-medium hover:bg-[#37393e] hover:bg-opacity-[7%] px-3 py-2.5  rounded-[13.5px] text-[15px] cursor-pointer duration-300 w-fit ",
+  };
+
   return (
-    <div className="absolute z-10 left-0 right-0 flex items-center justify-between">
+    <nav className={style.container}>
       <a href="/">
-        <img src="/images/logo.svg" alt="" className="w-[12rem] p-7" />
+        <img src="/images/logo.svg" alt="" className="w-[11rem] p-5 " />
       </a>
-      <p className="font-Gottham-medium mr-7 bg-[#37393e] bg-opacity-20 px-3 py-1.5 text-sm rounded-[10px] text-base cursor-pointer">
-        Menu
-      </p>
-    </div>
+
+      <div className="w-[42rem] justify-between hidden xl:flex ">
+        <p className={style.navEle + " hidden xl:block "}>Model S</p>
+        <p className={style.navEle + " hidden xl:block"}>Model 3</p>
+        <p className={style.navEle + " hidden xl:block"}>Model X</p>
+        <p className={style.navEle + " hidden xl:block"}>Model Y</p>
+        <p className={style.navEle + " hidden xl:block"}>Solar Roof</p>
+        <p className={style.navEle + " hidden xl:block"}>Solar Panels</p>
+      </div>
+      <div className="flex justify-around w-fit xl:w-64">
+        <p className={style.navEle + " hidden xl:block"}>Shop</p>
+        <p className={style.navEle + " hidden xl:block"}>Account</p>
+        <p
+          className={
+            style.navEle + " bg-[#37393e] bg-opacity-10 xl:bg-transparent"
+          }
+        >
+          Menu
+        </p>
+      </div>
+    </nav>
   );
 }
 
